ce611f6d6cb9b9fc62ed5e337d88196bceadc317
Josue <val171001@uvg.edu.gt>
2019-05-12 23:30:25 -0600

    Add cors suport

--- server.js
+++ server.js
@@ -11,6 +11,17 @@ app.use(
   })
 )
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Methods', 'DELETE, PUT');
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  if ('OPTIONS' == req.method) {
+     res.sendStatus(200);
+   }
+   else {
+     next();
+   }});
+
 app.get('/', (request, response) => {
     response.json({ info: 'Node.js, Express, and Postgres API' })
   })
